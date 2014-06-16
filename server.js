293f11551d3bd2e090d0a83c2a904b746f25ebb9
Youlong Li <chongzizil@gmail.com>
2014-06-16 10:39:11 -0400

    module 3-4
    :) That’s the easy part

--- server.js
+++ server.js
@@ -24,18 +24,16 @@ app.configure(function () {
 	app.use(express.static(__dirname + '/public'));
 });
 
-mongoose.connect('mongodb://localhost/mean');
+if (env === "development") {
+	mongoose.connect('mongodb://localhost/mean');
+} else {
+	mongoose.connect('mongodb://mean:mean@ds043057.mongolab.com:43057/mean');
+}
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
 	console.log('mean db opened!');
-});
-
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-	mongoMessage = messageDoc.message;
 });
 
 app.get('/partials/:partialPath', function(req, res) {
@@ -43,9 +41,7 @@ app.get('/partials/:partialPath', function(req, res) {
 });
 
 app.get('*', function(req, res) {
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index', {});
 });
 
 var port = process.env.PORT || 3030;
